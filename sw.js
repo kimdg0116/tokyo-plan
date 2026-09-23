ff5b8547661d93762613ecefda4f11d652b644f4
Dong-Kyu Kim <dongkyu-kim@Dong-Kyuui-MacBookPro.local>
2026-09-23 15:48:24 +0900

    Add plan nearby location control and collapse tips

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 도쿄 플랜 — 오프라인 캐시 */
-var VERSION = "26.09.23-0008";
+var VERSION = "26.09.23-0009";
 var CACHE = "tp-v" + VERSION;
 var CORE = ["./", "./index.html", "./data.json", "./manifest.webmanifest",
             "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png"];
